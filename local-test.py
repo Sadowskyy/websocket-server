9418de53fa4cb7c824a7a1b5abe19e49c4979eb1
Wiktor Sadowski <58751887+Sadowskyy@users.noreply.github.com>
2022-11-26 02:19:32 +0100

    Delete local-test.py

--- local-test.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import websockets
-
-async def hello():
-    async with websockets.connect("ws://127.0.0.1:8080") as websocket:
-        await websocket.send("Sending data...")
-        await websocket.recv()
-
-asyncio.run(hello())
